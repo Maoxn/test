e461bec46b39484f86e1ed9918ef0f452653b5c8
郑天锴 <maoxn@zhengtiankaideMacBook-Pro.local>
2020-03-29 21:08:17 -0700

    Updates

---
+++ precache-manifest.0b02c3d6d56485a6aa20124f65171526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9c3bce87ead9913f8661fa8e8337ee",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "50e477b7c1b185392afe",
+    "url": "/test/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "761a901ac475b27cc1ca",
+    "url": "/test/static/js/2.486e8562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.486e8562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e477b7c1b185392afe",
+    "url": "/test/static/js/main.176e0bc7.chunk.js"
+  },
+  {
+    "revision": "a2a118daa8ca02cff825",
+    "url": "/test/static/js/runtime-main.5ebfacb6.js"
+  }
+]);
